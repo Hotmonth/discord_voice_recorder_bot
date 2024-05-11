b0f7ccf91e859bd477a4718ac4176a714787ead5
Hotmonth <ratadatax@gmail.com>
2024-05-12 04:14:28 +0500

    👷Setup logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/Hotmonth/discord_voice_recorder_bot/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
 	// TODO: Init logger
+	log := setupLogger(cfg.Env)
+	log = log.With(slog.String("env", cfg.Env))
+
+	log.Info("Starting the bot")
 
 	// TODO: Init storage
 
 	// TODO: Init bot
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
